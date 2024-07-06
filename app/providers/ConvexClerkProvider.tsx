a50b2574e5aeec3241c76cdaf50b18686e65a821
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-07 00:20:07 +0530

    added-users-https

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -3,10 +3,11 @@
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL  as string);
 
-const ConvexClerkProvider = ({ children} : { children: ReactNode }) => (
+const ConvexClerkProvider = ({ children} : { children:ReactNode }) => (
   
     <ClerkProvider>publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
