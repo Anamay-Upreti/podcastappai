65797204c2f59e5082d4f6a7228f4cf36f95b24c
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-10 22:47:50 +0530

    added-files

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,8 +1,28 @@
+import { GeneratePodcastProps } from '@/types'
+import { Label } from '@radix-ui/react-label'
 import React from 'react'
+import { Textarea } from './ui/textarea'
 
-const GeneratePodcast = () => {
+const GeneratePodcast = ({
+  setAudioStorageId
+  setAudio
+  voiceType
+  audio
+  voicePrompt
+  setVoicePrompt
+  setAudioDuration
+}: GeneratePodcastProps) => {
   return (
-    <div>GeneratePodcast</div>
+    <div>
+      <div className='flex flex-col gap-2.5'>
+        <Label className='text-16 font-bold text-white-1'>
+        AI import to Generate Podcast
+        </Label>
+       <Textarea
+       className='input-class font-light focus-visible:ring-orange-1'
+       />
+      </div>
+    </div>
   )
 }
 
